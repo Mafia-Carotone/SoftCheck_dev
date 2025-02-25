e299f885476869ba8b204cdb4997c7b04602a445
softcheck.app <softcheck.app@gmail.com>
2025-02-25 21:29:23 +0000

    idx_ok

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.docker-compose
+    pkgs.sudo
+    pkgs.docker
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
